7d72b35230c246d850028a8ea3a8ac3a6b0d7477
Efloud <68462068+Efloud@users.noreply.github.com>
2022-05-24 15:48:31 +0300

    Create status.py

---
+++ status.py
@@ -0,0 +1,37 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor
+save_domain_list = []
+
+
+def req(domain):
+    try:
+        url = requests.get(f"https://{domain}", timeout=0.5)
+        if url.status_code == 200:
+            save_domain_list.append(domain)
+            print("[200] ", domain)
+        if url.status_code == 403:
+            save_domain_list.append(domain) 
+            print("[403] ", domain)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+def main():
+    domains_location = input("Domain Listesi Girin : ")
+    liss = []
+    with open(domains_location, encoding="utf-8") as d:
+        oku = d.read()
+        split = oku.splitlines()
+        for i in split:
+            liss.append(i)
+    with ThreadPoolExecutor(max_workers=200) as executor:
+        executor.map(req, liss)
+
+
+def save_to_file(content, location_filename=r"C:\Users\efloud1\Desktop\output.txt"): # Change Location Path
+    with open(location_filename, 'w') as files:
+        print(*save_domain_list , file=files, sep="\n")
+
+
+main()
+save_to_file(save_domain_list)
