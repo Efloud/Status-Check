1433855f4b589dddffd105e7cbe0ad603c80d7c4
Efloud <68462068+Efloud@users.noreply.github.com>
2022-05-24 19:31:16 +0300

    Update status.py

--- status.py
+++ status.py
@@ -1,24 +1,24 @@
-#!/usr/bin/python
 import requests
 from concurrent.futures import ThreadPoolExecutor
-x = []
+from termcolor import colored
+save_domain_list = []
 
 
 def req(domain):
     try:
         url = requests.get(f"https://{domain}", timeout=0.5)
         if url.status_code == 200:
-            x.append(domain)
-            print("[200] ", domain)
+            save_domain_list.append(domain)
+            print (colored("[200]", "green"), "https://"+ domain)
         if url.status_code == 403:
-            x.append(domain) 
-            print("[403] ", domain)
+            save_domain_list.append(domain) 
+            print (colored("[403]", "red"),"https://"+domain)
     except requests.exceptions.ConnectionError:
         pass
 
 
 def main():
-    domains_location = input("Domain Listesini Sürükleyin. : ")
+    domains_location = input("Domain Listesi Girin : ")
     liss = []
     with open(domains_location, encoding="utf-8") as d:
         oku = d.read()
@@ -30,9 +30,9 @@ def main():
 
 
 def save_to_file(content, save_location=r"output.txt"):
-    with open(save_location, 'w') as files:
-        print(*content , file=files, sep="\n")
+    yaz = open(save_location, 'w')
+    print(*content , file=yaz, sep="\n")
 
 
 main()
-save_to_file(x)
+save_to_file(save_domain_list)
