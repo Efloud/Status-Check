0631dec507da2cdb9967a1ab4441658a20ad06fd
Efloud <68462068+Efloud@users.noreply.github.com>
2022-05-24 19:59:09 +0300

    Update status.py

--- status.py
+++ status.py
@@ -1,23 +1,25 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from termcolor import colored
-save_domain_list = []
-
+responce_200 = []
+responce_403 = []
 
 def req(domain):
+
     try:
         url = requests.get(f"https://{domain}", timeout=0.5)
         if url.status_code == 200:
-            save_domain_list.append(domain)
+            responce_200.append(domain)
             print (colored("[200]", "green"), f"https://{domain}")
         if url.status_code == 403:
-            save_domain_list.append(domain) 
+            responce_403.append(domain) 
             print (colored("[403]", "red"), f"https://{domain}")
     except requests.exceptions.ConnectionError:
         pass
 
 
 def main():
+
     domains_location = input("Domain Listesi Girin : ")
     liss = set()
     with open(domains_location, encoding="utf-8") as d:
@@ -29,10 +31,15 @@ def main():
         executor.map(req, liss)
 
 
-def save_to_file(content, save_location=r"output.txt"):
+def save_200(content, save_location=r"200.txt"):
     yaz = open(save_location, 'w')
     print(*content , file=yaz, sep="\n")
 
 
+def save_403(content, save_location=r"403.txt"):
+    yaz = open(save_location, 'w')
+    print(*content , file=yaz, sep="\n")
+
 main()
-save_to_file(save_domain_list)
+save_200(responce_200)
+save_403(responce_403)
