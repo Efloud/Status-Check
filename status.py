6c10fb5edc5e113f837a34f53bf76a617bad668d
Efloud <68462068+Efloud@users.noreply.github.com>
2022-05-28 02:40:49 +0300

    Update status.py

--- status.py
+++ status.py
@@ -1,8 +1,7 @@
-import requests
+import requests, os, pyfiglet
 from concurrent.futures import ThreadPoolExecutor
 from termcolor import colored
-import os 
-import pyfiglet
+
 
 banner = pyfiglet.figlet_format("EfStatusCheck")
 
@@ -30,8 +29,9 @@ def req(domain):
 def main():
 
     print(colored(banner, "red"), end="")
+    print("-"*40, sep="")
     domains_location = input("Domain Listesi Girin : ")
-    print("\n", end="")
+    print("-"*40, "\n", sep="", end="")
     liss = set()
     global toplam
     with open(domains_location, encoding="utf-8") as d:
@@ -43,22 +43,22 @@ def main():
     with ThreadPoolExecutor(max_workers=200) as executor:
         executor.map(req, liss)
         
-    print("\n")
+    print("-"*40)
     print("Toplam Taranan Sayısı: ", colored(f"[{toplam}]", "red"), sep="")
 
 
 def cikti_kontrol(s_200, s_403, location=f"output.txt"):
     
-    sorgu = input("Almak istediğiniz Çıktıyı giriniz => [200/403]: ")
+    sorgu = input("Almak istediğiniz => [200/403]: ")
     if sorgu == "200":
         with open(location, "w", encoding="utf-8") as s200:
             print(*s_200, file=s200, sep="\n")
-            print(f"Kayıt Yeri : {os.getcwd()}")  
+            print(f"Kayıt Yeri : {os.getcwd()}", "\n", "-"*40 , sep="")  
             
     elif sorgu == "403":
         with open(location, "w", encoding="utf-8") as s403:
             print(*s_403, file=s403, sep="\n")
-            print(f"Kayıt Yeri : {os.getcwd()}")  
+            print(f"Kayıt Yeri : {os.getcwd()}", "\n", "-"*40 , sep="")  
     
     else:
         print("Lütfen 200 veya 400 Çıktısını Almayı DENEYİN!")
